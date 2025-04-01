1f4a662447c1f0ba2dccf7dbe7a6d0b59598061b
Raynell Rajeev <108294211+raynellrajeev@users.noreply.github.com>
2025-04-02 01:36:14 +0530

    collect disk usage data

--- django-app/monitor/models.py
+++ django-app/monitor/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 class SystemMetrics(models.Model):
@@ -6,5 +7,15 @@ class SystemMetrics(models.Model):
     cpu_usage = models.FloatField()
     memory_usage = models.FloatField()
     
+    class Meta:
+        ordering = ['-timestamp']
+
+class DiskUsage(models.Model):
+    timestamp = models.DateTimeField(default=timezone.now)
+    drive = models.CharField(max_length=10)
+    usage_percent = models.FloatField()
+    total_space = models.BigIntegerField()
+    used_space = models.BigIntegerField()
+
     class Meta:
         ordering = ['-timestamp']
--- django-app/monitor/urls.py
+++ django-app/monitor/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from .views import (SystemMetricsList, CurrentSystemMetrics, 
-                   StartMonitoring, StopMonitoring)
+                   StartMonitoring, StopMonitoring, DiskUsageList, CurrentDiskUsage)
 
 urlpatterns = [
     path('metrics/', SystemMetricsList.as_view(), name='metrics-list'),
     path('metrics/current/', CurrentSystemMetrics.as_view(), name='current-metrics'),
     path('monitor/start/', StartMonitoring.as_view(), name='start-monitoring'),
     path('monitor/stop/', StopMonitoring.as_view(), name='stop-monitoring'),
+    path('disk/', DiskUsageList.as_view(), name='disk-list'),
+    path('disk/current/', CurrentDiskUsage.as_view(), name='current-disk'),
 ]
