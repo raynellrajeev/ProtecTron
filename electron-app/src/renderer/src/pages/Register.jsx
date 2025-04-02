d76a0bb26d6929d9f8a24a680af03236247f52ee
Raynell Rajeev <108294211+raynellrajeev@users.noreply.github.com>
2025-04-02 12:54:02 +0530

    remove comments

--- electron-app/src/renderer/src/pages/Register.jsx
+++ electron-app/src/renderer/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import axios from 'axios' // ✅ Import axios
+import axios from 'axios' 
 import { FaUser, FaLock } from 'react-icons/fa'
 import Logo from '../assets/images/Logo.png'
 import { Link, useNavigate } from 'react-router-dom'
